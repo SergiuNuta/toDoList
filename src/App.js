c102389510e5fd2eee11f02ea8e9bdac16e56acc
Serghei Nuta <sergiu.nuta@gmail.com>
2021-06-21 20:25:18 +0100

    fixing form css style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 const App = () => {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <Form />
+      <header>
+        <h1>Serghei's Todo List</h1>
       </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Form.css';
 
 const Form = () => {
+    const inputTextHandler = e => {
+        console.log(e.target.value);
+    }
     return (
         <form>
-            <input type="text" className="todo-input" />
+            <input onChange={inputTextHandler} type="text" className="todo-input" />
             <button className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
